b3befd5186bc5b4facf28f8d485b1c8569f42594
JaeJoonSim <104071482+JaeJoonSim@users.noreply.github.com>
2024-11-02 18:35:48 +0900

    Fix Slip Anim, Effect

--- Assets/Scripts/SnowStormTrigger.cs
+++ Assets/Scripts/SnowStormTrigger.cs
@@ -14,17 +14,24 @@ namespace BlueRiver
 
         [SerializeField] private ParticleSystem snowStormParticle;
 
+        List<ParticleSystem.Particle> inside = new List<ParticleSystem.Particle>();
+
         private void Start()
         {
             player = GameManager.Instance.player;
             playerCol = player.GetComponent<Collider2D>();
             ps = GetComponent<ParticleSystem>();
+
+            if (player != null)
+                ps.trigger.AddCollider(player);
         }
 
         private void OnParticleCollision(GameObject other)
         {
             if (other == player.gameObject)
+            {
                 player.SetInSnowStorm(true);
+            }
         }
     }
 }
