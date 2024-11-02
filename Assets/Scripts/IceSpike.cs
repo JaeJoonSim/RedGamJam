b26c3785f2b2396fc820a9680b77ebdaeb9f58e9
JaeJoonSim <104071482+JaeJoonSim@users.noreply.github.com>
2024-11-03 01:46:12 +0900

    IceSpark

---
+++ Assets/Scripts/IceSpike.cs
@@ -0,0 +1,33 @@
+using BlueRiver;
+using BlueRiver.Character;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSpike : MonoBehaviour
+{
+    private PlayerController player;
+    private Rigidbody2D playerRb;
+
+    private void Start()
+    {
+        player = GameManager.Instance.player;
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Vector2 contactPoint = collision.ClosestPoint(transform.position);
+            Vector2 center = transform.position;
+
+            if (contactPoint.y > center.y && playerRb.velocity.y < 0)
+            {
+                if (player.GetTree() != null)
+                    player.GetTree().TakeDamage();
+                Debug.LogError("123");
+            }
+        }
+    }
+}
